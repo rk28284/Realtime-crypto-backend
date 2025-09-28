cafac387e24aed6075a32f224316e60e81d4da41
rk28284 <112754760+rk28284@users.noreply.github.com>
2025-09-28 22:48:31 +0530

    completed backend

--- route/trade.routes.js
+++ route/trade.routes.js
@@ -3,25 +3,15 @@ const tradeRouter = express.Router();
 const {
   getAllTrades,
   getTradeById,
-  posttrade,
-  deleteTrade,
-  updateTrade,
 } = require("../controllers/trade.controller");
-const authentication = require("../middleware/auth.middleware");
+
 
 // Get all trades
-tradeRouter.get("/trade", authentication, getAllTrades);
+tradeRouter.get("/trade", getAllTrades);
 
 // Get a single trade by ID
-tradeRouter.get("/trade/:id", authentication, getTradeById);
+tradeRouter.get("/trade/:id", getTradeById);
 
-//post
-tradeRouter.post("/trade/add", authentication, posttrade);
 
-//delete
-tradeRouter.delete("/trade/delete/:id", authentication, deleteTrade);
-
-//update
-tradeRouter.patch("/trade/update/:id", authentication, updateTrade);
 
 module.exports = tradeRouter;
